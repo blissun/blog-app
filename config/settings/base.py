b297d63ad79c300f448084e7d71b4cc07d317884
blissun <hello-ryan@kakao.com>
2021-02-05 01:06:28 +0900

    modified users.models.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -26,6 +26,7 @@ PROJECT_APPS = [
 
 THIRD_PARTY_APPS = [
     'taggit',
+    'imagekit',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
--- users/models.py
+++ users/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 
 class User(AbstractUser):
-    """ Custom User model """
+    profile_image = ProcessedImageField(
+        upload_to="user/profile_image",
+        processors=[ResizeToFill(600, 600)],
+        format='JPEG',
+        options={'quality': 80},
+    )
+
+    def __str__(self):
+        return self.username
